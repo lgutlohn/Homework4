0d66b0b69f80b9220cccfd49a87b3ee012d93763
Lauren Gutlohn <lgutlohn@gmail.com>
2014-02-24 09:02:25 -0500

    draft one of rube goldberg (ideas greater than results)

---
+++ Assets/Scripts/destroyTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyTrigger : MonoBehaviour {
+
+	public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {  
+			Application.LoadLevel (0);  
+		}  
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Destroy(other.gameObject);
+		if(other.gameObject){
+			if(sphere){
+				Instantiate(sphere, (transform.position), transform.rotation);
+			}
+			Destroy(gameObject);
+		}
+	}
+}
+
